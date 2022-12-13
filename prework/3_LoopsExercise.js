9b5d0b8b6ad6c952d5a09c89528bd44b7e7c894f
Anneice Manzanares <mountains.stars@gmail.com>
2022-12-12 17:34:20 -0700

    Finished 3_LoopsExercise

--- prework/3_LoopsExercise.js
+++ prework/3_LoopsExercise.js
@@ -11,23 +11,93 @@
 
 // Exercise 1. Write a 'while' loop that prints the integers (whole numbers)
 // between 1 and 5 (inclusive).
+let i=1
+while (i <=5) {
+  console.log(i)
+  i++
+}
 
 // Exercise 2. Write a 'do while' loop that prints the integers (whole numbers)
 // between 1 and 5 (inclusive).
+let j = 1;
+do {
+  console.log(j);
+  j++;
+} while (j <= 5);
 
 // Exercise 3. Write a 'for' loop that prints the integers (whole numbers)
 // between 1 and 5 (inclusive).
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
 
 // Exercise 4. Now we want a loop that prints the integers
 // counting DOWN from 10 to 1 (inclusive). Write three loops that do this, to practice
 // writing loops in all 3 ways -- as a 'while', 'do while', and 'for' loop.
 
+//while loop
+let k = 10;
+while (k >= 1) {
+  console.log(k);
+  k--;
+}
+// do while loop
+let l = 10;
+do {
+  console.log(l);
+  l--;
+} while (l >= 1);
+
+// for loop
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
+}
+
+
 // Exercise 5. Write a loop that prints the integers from 7 to 27. Write this
 // loop in all 3 ways -- as a 'while', 'do while', and 'for' loop.
+
+// while loop
+let m = 7;
+while (m <= 27) {
+  console.log(m);
+  m++;
+}
+
+// do while loop
+let n = 7;
+do {
+  console.log(n);
+  n++;
+} while (n <= 27);
+
+// for loop
+for (let i = 7; i <= 27; i++) {
+  console.log(i);
+}
 
 // Exercise 6. Write a loop that prints numbers between 0 and 100, counting by tens.
 // I.e. it will print 10, then 20, then 30, etc.
 // Write this loop in all 3 ways -- as a 'while', 'do while', and 'for' loop.
+
+// while loop
+let p = 0;
+while (p <= 100) {
+  console.log(p);
+  p+=10;
+}
+
+// do while loop
+let q = 0;
+do {
+  console.log(q);
+  q+=10;
+} while (q <= 100);
+
+// for loop
+for (let i = 0; i <= 100; i+=10) {
+  console.log(i);
+}
 
 // Exercise 7. Add a comment as to why the following loop is an infinite loop (will
 // run without ever stopping). Then fix the loop so that it stops when
@@ -37,10 +107,24 @@ let counterFour = 1;
 while (counterFour < 2) {
   console.log("HELP ME!");
   counterFour--;
-}
+} // This is an infinite loop because the condition (counterFour <2) will always be true since counterFour starts at 1 and has no ending condition.
+
+//new loop
+ //let counterFour= 1;
+ while (counterFour >= -100) {
+  console.log('HELP ME!')
+  counterFour--
+ }
 
 // Exercise 8. Make a variable that contains your favorite integer. Write a loop
 // (your choice which type) that prints the integers from 0 to that number.
+
+let myFavNum = 24
+let a =1
+while (a <= myFavNum) {
+  console.log (a)
+  a++
+}
 
 // Exercise 9. Make a variable that contains your favorite integer (this time make sure it's
 // less than 100). Write a loop (your choice which type) that prints the integers from 0 to 100.
@@ -53,11 +137,27 @@ while (counterFour < 2) {
 // ...
 // (Hint - use an if statement in your loop)
 
+let myNum = 24;
+let b = 0;
+while (b <= 100) {
+  if (b === myNum) {
+    console.log(b + " my favorite number!");
+  } else {
+    console.log(b + " not my favorite number");
+  }
+  b++;
+}
+
 // Exercise 10. In some of the exercises above, we had you write all 3 types of loops, for practice.
 // But in real life, how would you decide which type of loop to use? You might not have an
 // exact answer, but spend a few minutes thinking about the different types of loops and/or
 // doing some research and write down your thoughts in a comment below:
 //
+
+  /* 
+  A "for loop" should be used if you know exactly how many times the loop should run;
+  A "while loop" should be used if you want the running/breaking of the loop to be based on a condition, rather than number of times; and A "do while loop" is similar to "while loop" except that a do while loop will always run at least once because the condition is evaluated after executing the statement. 
+  */
 
 // Exercise 11. Now we'll practice using nested loops (a loop inside another loop)!
 // Update the nested loops below to so that for each value of outsideCounter,
@@ -80,10 +180,10 @@ inside 1
 *********************************** 
 */
 
-for (let outsideCounter = 0; outsideCounter <= 3 /*???*/; ) {
+for (let outsideCounter = 0; outsideCounter <= 3; outsideCounter++ ) {
   console.log("counting down from", outsideCounter);
-  for (let insideCounter = outsideCounter /*???*/ /*???*/; ; ) {
-    console.log("inside" /*???*/);
+  for (let insideCounter = outsideCounter; insideCounter >= 1; insideCounter--) {
+    console.log("inside"+ " " + insideCounter);
   }
   console.log("***********************************");
 }
