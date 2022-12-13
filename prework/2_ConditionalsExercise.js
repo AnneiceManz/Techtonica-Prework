ca500071eb55d3435eb9e8b956ef9c8226f1eec8
Anneice Manzanares <mountains.stars@gmail.com>
2022-12-12 17:03:50 -0700

    Finished 2_ConditionalsExercise

--- prework/2_ConditionalsExercise.js
+++ prework/2_ConditionalsExercise.js
@@ -55,7 +55,10 @@ function exercise1(num1) {
   // ------------------------------------------
   // Write your code for exercise 1 below here:
   // ------------------------------------------
-
+    answer1= "num1 is small"
+    if (num1 >10) {
+      answer1= `The value of num1 is ${num1} and is greater than 10`
+    }
   // ------------------------------------------
   // And above here
   // ------------------------------------------
@@ -77,7 +80,11 @@ function exercise2(num2) {
   // --------------------------------------------
   // Write your code for the exercise below here:
   // --------------------------------------------
-
+    if (num2 % 2 === 0 ) {
+      answer2 = `${num2} is even`
+    } else {
+      answer2 = `${num2} is odd`
+    }
   // --------------------------------------------
   // And above here
   // --------------------------------------------
@@ -98,7 +105,13 @@ function exercise3(num3) {
   // --------------------------------------------
   // Write your code for the exercise below here:
   // --------------------------------------------
-
+  if (num3 === 0 ) {
+    answer3 = `${num3} is zero`
+  } else if (num3 % 2 === 0) {
+    answer3 = `${num3} is even`
+  } else {
+    answer3= `${num3} is odd`
+  }
   // --------------------------------------------
   // And above here
   // --------------------------------------------
@@ -116,7 +129,11 @@ function exercise4(varA, varB) {
   // --------------------------------------------
   // Write your code for the exercise below here:
   // --------------------------------------------
-
+if (varA === varB) {
+  answer4= 'varA and varB are equal'
+} else {
+  answer4= 'varA and varB differ'
+}
   // --------------------------------------------
   // And above here
   // --------------------------------------------
@@ -128,6 +145,8 @@ function exercise4(varA, varB) {
 // seemed like they are equal, but would not pass the tests? In your analysis
 // consider other data types beside strings and variables.
 
+    /* Answer: The unexpected case would happen when the two variables have the same value but are of different data types. Exercise4 used strict equality and will return "varA and varB differ". For example, varA = 1 and varB = "1". For another example with boolean values, exercise4(0, false) would return false so the test would not pass, but  at first glance 0 is a faulsy value. The reason the test fails is that varA and varB are of different data types here - number and boolean, respectively. */
+
 // EXERCISE 6.
 // Here, assign the value of true to answer6 if:
 // `varA` and `varB` are equal, but `varA` or `varB` does not equal `varC`
@@ -137,7 +156,11 @@ function exercise6(varA, varB, varC) {
   // --------------------------------------------
   // Write your code for the exercise below here:
   // --------------------------------------------
-
+  if (varA === varB && varA !== varC || varB !== varC ) {
+    answer6= true
+  } else {
+    answer6= false
+  }
   // --------------------------------------------
   // And above here
   // --------------------------------------------
@@ -162,7 +185,22 @@ function exercise7(num7) {
   // --------------------------------------------
   // Write your code for the exercise below here:
   // --------------------------------------------
-
+  switch (num7) {
+    case 1:
+      answer7 = "You won!";
+      break;
+    case 7:
+      answer7 = "You are lucky!";
+      break;
+    case 101:
+      answer7 = "Welcome to coding 101!"
+      break;
+    case 1000000:
+      answer7 = "You are one in a million!";
+      break;
+    default:
+      answer7 = "Thanks for that!"
+  }
   // --------------------------------------------
   // And above here
   // --------------------------------------------
@@ -179,7 +217,11 @@ function exercise8(amount1, amount2, minimum, maximum) {
   // --------------------------------------------
   // Write your code for the exercise below here:
   // --------------------------------------------
-
+  if (amount1 > minimum && amount1 < maximum && amount2 > minimum && amount2 < maximum) {
+    answer8 = true;
+  } else {
+    answer8 = false;
+  }
   // --------------------------------------------
   // And above here
   // --------------------------------------------
@@ -198,7 +240,26 @@ function exercise9(item) {
   // --------------------------------------------
   // Write your code for the exercise below here:
   // --------------------------------------------
-
+  if (typeof item === 'number') {
+    switch (item) {
+      case 1:
+        answer9 = "You won!";
+        break;
+      case 7:
+        answer9 = "You are lucky!";
+        break;
+      case 101:
+        answer9 = "Welcome to coding 101!"
+        break;
+      case 1000000:
+        answer9 = "You are one in a million!";
+        break;
+      default:
+        answer9 = "Thanks for that!"
+    }
+  } else {
+    answer9 = `Please send a number, that was a ${typeof item}.`;
+  }
   // --------------------------------------------
   // And above here
   // --------------------------------------------
@@ -220,7 +281,25 @@ function exercise10(num10) {
   // --------------------------------------------
   // Write your code for the exercise below here:
   // --------------------------------------------
-
+  function exercise10(num10) {
+    let answer10;
+    // --------------------------------------------
+    // Write your code for the exercise below here:
+    // --------------------------------------------
+    if (num10 % 15 === 0) {
+      answer10 = "Fizz Buzz";
+    } else if (num10 % 3 === 0) {
+      answer10 = "Fizz";
+    } else if (num10 % 5 === 0) {
+      answer10 = "Buzz";
+    } else {
+      answer10 = num10;
+    }
+    // --------------------------------------------
+    // And above here
+    // --------------------------------------------
+    return answer10;
+  }
   // --------------------------------------------
   // And above here
   // --------------------------------------------
